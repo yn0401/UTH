2284d7742fd69df0c3c0bec6cec04799f7618054
hduy12 <duy.lh3600@sinhvien.hoasen.edu.vn>
2022-12-11 21:40:57 +0700

    member redux

---
+++ redux/reducers/memberReducer.js
@@ -0,0 +1,57 @@
+import {
+  getAll,
+  getTop10,
+  getByID,
+  addNew,
+  update,
+  deleteMember,
+  searchMember,
+} from "../actions/memberAction";
+
+const initialState = {
+  members: [],
+  member: {},
+  top10Members: [],
+  searchMembers: [],
+};
+
+export const memberReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case getAll:
+      return {
+        ...state,
+        members: action.payload,
+      };
+    case getTop10:
+      return {
+        ...state,
+        top10Members: action.payload,
+      };
+    case getByID:
+      return {
+        ...state,
+        member: action.payload,
+      };
+    case addNew:
+      return {
+        ...state,
+        members: [...state.members, action.payload],
+      };
+    case update:
+      return {
+        ...state,
+      };
+    case deleteMember:
+      return {
+        ...state,
+        members: state.members.filter((member) => member.id !== action.payload),
+      };
+    case searchMember:
+      return {
+        ...state,
+        members: [...action.payload],
+      };
+    default:
+      return { ...state };
+  }
+};
